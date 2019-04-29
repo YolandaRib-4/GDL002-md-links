6fc2c5b83696c6f8cfb1af5c16f37b7b317e640d
Yolanda Ribas <yolanda.ribas@hotmail.com>
2019-04-29 18:34:24 -0500

    create read and parseLinks functions

---
+++ mdLinks.js
@@ -0,0 +1,43 @@
+const mdLinks = require('./index');
+
+const filePath = process.argv[2];
+
+const resultReadFile  = mdLinks(filePath, null);
+
+resultReadFile
+    .then(
+        (data)=> { 
+            const linkArray = parseLinks(data); //modifiq aqui 
+            const objArray = linkArray.map(extactData); // modifiq aquí 
+            console.log(objArray);
+        }
+    ).catch(
+        (err)=> { 
+            console.error(err);
+        }
+    );
+
+//Funcion para encontrar links en el archivo que leyó
+const parseLinks = (data) => {
+    const regExp = /\[(.+)\]\((.+)\)/g;
+    const matchLinks = data.match(regExp);
+    return matchLinks;
+};
+
+
+// Funcion que crea el objeto con texto y los enlaces
+const extactData = (mdLink) => {
+    const regExp = /\[(.+)\]\((.+)\)/g;
+    const obj = {};
+    const groups = regExp.exec(mdLink);  // matching groups 
+    // 0 [Node.js](https://nodejs.org/en/)'
+    // 1 Node.js
+    // 2 https://nodejs.org/en/
+
+    obj.text = groups[1];
+    obj.link = groups[2];
+    return obj;
+}
+
+
+
