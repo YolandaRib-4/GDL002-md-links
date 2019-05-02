89dba3ffd18e2fb16813fdb69b47cf262ad18599
Yolanda Ribas <yolanda.ribas@hotmail.com>
2019-05-01 23:57:34 -0500

    add function to validate links

--- mdLinks.js
+++ mdLinks.js
@@ -1,15 +1,37 @@
 const mdLinks = require('./index');
+const fetch = require('node-fetch');
+const chalk = require('chalk');
 
 const filePath = process.argv[2];
-
+const argument = process.argv[3];
+let validate = false;
+if(argument && (argument === '--validate' || argument === '-v')){
+    validate = true;
+}
 const resultReadFile  = mdLinks(filePath, null);
 
 resultReadFile
     .then(
         (data)=> { 
-            const linkArray = parseLinks(data); //modifiq aqui 
-            const objArray = linkArray.map(extactData); // modifiq aquí 
-            console.log(objArray);
+            const linkArray = parseLinks(data); 
+            const objArray = linkArray.map(extactData);
+            if(!validate) {
+                for(let obj of objArray) {
+                    console.log(chalk.cyan(obj.link), chalk.yellow(obj.text));
+                }
+                return;
+            }
+            for (let obj of objArray) {
+                fetch(obj.link)
+                .then(response => {
+                  if (response.status == 200) {
+                    console.log(chalk.green('[✔]'), chalk.cyan(obj.link), chalk.bgGreen(` ${response.status} ${response.statusText} `), chalk.yellow(obj.text));
+                  } else {
+                    console.log(chalk.red('[X]'), chalk.cyan(obj.link), chalk.bgRed(` ${response.status} ${response.statusText} `), chalk.white(obj.text ));
+                  }
+                })
+                .catch((error)=> console.log(chalk.gray('[-]'), chalk.cyan(obj.link), chalk.bgRed(` ${error.type} ${error.code} `), chalk.white(obj.text )));
+              }           
         }
     ).catch(
         (err)=> { 
@@ -24,7 +46,6 @@ const parseLinks = (data) => {
     return matchLinks;
 };
 
-
 // Funcion que crea el objeto con texto y los enlaces
 const extactData = (mdLink) => {
     const regExp = /\[(.+)\]\((.+)\)/g;
@@ -34,19 +55,7 @@ const extactData = (mdLink) => {
     // 1 Node.js
     // 2 https://nodejs.org/en/
 
-    obj.text = groups[1];
+    obj.text = groups[1].substring(0, 30);
     obj.link = groups[2];
     return obj;
 };
-
-// Funcion que valida que la url sea un link valido
-const validateLinks = (link) => {
-   const regExp = /(?:https?:\/\/)?(?:www\.)?[a-z0-9-]+\.[a-z]{2,5}/g
-   const validLink = link.match(regExp);
-   return validLink; 
-}
-
-
-
-
-
