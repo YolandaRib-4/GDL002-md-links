88878fcfd2021477773e81ced76c8fe2fc1122a6
Yolanda Ribas <yolanda.ribas@hotmail.com>
2019-05-05 19:44:07 -0500

    update mdLinks require path in cli.js

--- cli.js
+++ cli.js
@@ -1,9 +1,3 @@
 #!/usr/bin/env node
 
-let command = null;
-
-try {
-    command = require(`${process.cwd()}/node_modules/mdlinks`);
-} catch (e) {
-    command = require("mdlinks");
-}
+require('./mdLinks');
